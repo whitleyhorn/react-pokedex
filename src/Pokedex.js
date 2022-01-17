ea3c5aeacfd48918becf85fd5c549786e3516af7
Whitley Horn <whitleyblaine@protonmail.com>
2022-01-17 14:54:23 -0500

    Set up super basic App.js and Pokedex.js

---
+++ src/Pokedex.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Pokedex extends Component {
+    render(){
+        return (
+            <div>hi</div>
+        );
+    }
+}
+
+export default Pokedex;
